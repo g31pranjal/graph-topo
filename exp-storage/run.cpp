072a3f8ed564722d0e17c36abc9d0adfd78aca43
g31pranjal <g31.pranjal@gmail.com>
2018-09-27 23:49:58 -0400

    run.cpp according new convention

--- exp-storage/run.cpp
+++ exp-storage/run.cpp
@@ -12,22 +12,22 @@ int main() {
 
 	csr ob;
 	
-	int t;
-	cin >> t;
-	int a = t;
+	int startNode, endNode, edges;
+	cin >> startNode >> endNode >> edges;
+	
+	int a = edges;
 
 	auto tb = chrono::steady_clock::now();
-	for(int i=1;i<=t;i++) {
+	
+	for(int i = 1; i <= edges; i++) {
 		int s, d;
 		cin >> s >> d;
 		ob.insert(s, d);
-		if(i%500 == 0) {
+		if(i%100 == 0) {
 			auto ta = chrono::steady_clock::now();
 			auto diff = ta - tb;
 			tb = ta;
-			// cout << i << ", " << t <<  "\n";
 			cout << chrono::duration <double, milli> (diff).count()*1000 << "\n";
-		
 		}
 	}
 
